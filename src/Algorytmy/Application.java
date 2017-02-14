fd19e0334d8b372d6add4d7e7e6cf1b22257c190
unknown <k.maczkowska@gmail.com>
2017-02-14 18:08:51 +0100

    Initial commit

---
+++ src/Algorytmy/Application.java
@@ -0,0 +1,10 @@
+package Algorytmy;
+
+/**
+ * Created by RENT on 2017-02-14.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
