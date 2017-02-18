00f61fbf8b21c242583f7ca2ec57956f49a01cb0
unknown <k.maczkowska@gmail.com>
2017-02-18 12:29:30 +0100

    Added package queue with FIFIQueue interface. Added FIFOQueueTests class.

---
+++ src/test/algorytmy/structures/queue/FIFOQueueTests.java
@@ -0,0 +1,35 @@
+package test.algorytmy.structures.queue;
+
+import algorytmy.structures.queue.FIFOQueue;
+import algorytmy.structures.queue.Queue;
+
+/**
+ * Created by RENT on 2017-02-18.
+ */
+public class FIFOQueueTests {
+    public static void main(String[] args) {
+        Queue queue = new FIFOQueue();
+        queue.push(9);
+        queue.push(4);
+        queue.push(7);
+        queue.push(1);
+        System.out.println(queue);
+        System.out.println(queue);
+
+        Queue clonedQueue = queue.clone();
+        System.out.println(clonedQueue);
+        clonedQueue.pop();
+        System.out.println(queue);
+        System.out.println(clonedQueue);
+
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        queue.push(8);
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+    }
+}
