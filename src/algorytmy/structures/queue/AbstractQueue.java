2858c6f2b7f9abde1c9b23de267d78ba24535605
unknown <k.maczkowska@gmail.com>
2017-02-18 14:31:01 +0100

    Added abstract class

---
+++ src/algorytmy/structures/queue/AbstractQueue.java
@@ -0,0 +1,26 @@
+package algorytmy.structures.queue;
+
+/**
+ * Created by RENT on 2017-02-18.
+ */
+public abstract class AbstractQueue implements Queue {
+
+    protected int value2;
+
+    private int test;
+
+    @Override
+    public void push(int value) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public Queue clone() {
+        return null;
+    }
+}
